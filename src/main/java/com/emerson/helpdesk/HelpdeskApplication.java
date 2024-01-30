49f0cead7678e83aced87ab86a28d9fbd179ef84
emerson cardoso <emerson.cardoso.trindade@gmail.com>
2024-01-29 21:22:42 -0300

    projeto iniciado

---
+++ src/main/java/com/emerson/helpdesk/HelpdeskApplication.java
@@ -0,0 +1,13 @@
+package com.emerson.helpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpdeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpdeskApplication.class, args);
+	}
+
+}
